6004f418f530727dda1d0995e4cea9d93a509037
Leandro Climaco <climacobnu@gmail.com>
2022-02-11 18:37:18 -0300

    Update projeto1.js

--- projeto1.js
+++ projeto1.js
@@ -1,48 +1,49 @@
-var prompt = require("prompt-sync")();
+var prompt = require('prompt-sync')();
+
+console.log("historia do personagem")
 
 
-let cont = 0;
-let a;
-let b;
-let c;
-let d;
-let e;
-
-
-//var s = new Boolean(true);      // valor inicial true
-//var n = new Boolean(false);         // valor inicial false
-
-
-
-
-while(cont < 1){
-    a = +prompt(`${cont+1} - Você salvou ... ? : `);
-    b = +prompt(`${cont+2} - Você salvou ... ? : `);
-    c = +prompt(`${cont+3} - Você salvou ... ? : `);
-    d = +prompt(`${cont+4} - Você salvou ... ? : `);
-    e = +prompt(`${cont+5} - Você salvou ... ? : `);
- 
-
-
-
-    if  ( a + b + c + d + e == 0 ) {
-     console.log("Você falha miseravelmente");
-
-    } else if ( a + b + c + d + e == 1 ||  a + b + c + d + e == 2 ) {
-     console.log(" Você falha, mas ainda consegue fugir da situação.");
-    } else if ( a + b + c + d + e == 3 ) {
-        console.log(" Você chega perto de conseguir alcançar seu objetivo, mas acaba falhando por pouco");
-    } else if ( a + b + c + d + e == 4) {
-        console.log(" Depois de muito esforço você conquista seu objetivo, embora não de maneira perfeita");
-    } else if (a + b + c + d + e >= 5) {
-        console.log(" Você triunfa de maneira inquestionável e seus feitos serão lembrados por muitas gerações.");
-        cont++
-   }
-
+var i = 0;
+var a = prompt("Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?")
+var b = a.toLowerCase()
+var b = b.trim()
+if (b === "sim"){
+    i++;
+} 
+var a = prompt("Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?")
+var b = a.toLowerCase()
+var b = b.trim()
+if (b === "sim"){
+    i++;
+} 
+var a = prompt("Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?")
+var b = a.toLowerCase()
+var b = b.trim()
+if (b === "sim"){
+    i++;
+} 
+var a = prompt("Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?")
+var b = a.toLowerCase()
+var b = b.trim()
+if (b === "sim"){
+    i++;
+} 
+var a = prompt("Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?")
+var b = a.toLowerCase()
+var b = b.trim()
+if (b === "sim"){
+    i++;
 }
 
 
-
-
-
-
+if (i > 4){
+    console.log("Você triunfa de maneira inquestionável e seus feitos serão lembrados por muitas gerações.")
+}else if (i > 3){
+    console.log("Depois de muito esforço você conquista seu objetivo, embora não de maneira perfeita.")
+}else if (i > 2){
+    console.log("Você chega perto de conseguir alcançar seu objetivo, mas acaba falhando por pouco.")
+}else if (i > 1){
+    console.log("Você falha, mas ainda consegue fugir da situação") 
+}else{
+    console.log("Você falha miseravelmente.")
+}
